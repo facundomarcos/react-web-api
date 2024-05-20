5013433d6d2b0099869484f53bc042143fbf486f
facundomarcos <facundomarcos@live.com.ar>
2024-05-20 00:08:26 -0300

    componente productos

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import theme from './theme/theme';
 import RegistrarUsuario from './componentes/seguridad/RegistrarUsuario';
 import MenuAppBar from './componentes/navegacion/MenuAppBar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Libro from './componentes/pantallas/Libro';
+import Productos from './componentes/pantallas/Productos';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
     <Switch>
       <Route exact path="/login" component={Login}/>
       <Route exact path="/registrar" component={RegistrarUsuario}/>
-      <Route exact path="/" component={Libro}/>
+      <Route exact path="/" component={Productos}/>
     </Switch>
   </Router>
 
