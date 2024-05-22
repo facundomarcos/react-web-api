c4d887760e35cab3828e1e554261c53ef0d9a361
facundomarcos <facundomarcos@live.com.ar>
2024-05-22 10:52:26 -0300

    componente perfil parte 1

--- src/componentes/seguridad/Perfil.js
+++ src/componentes/seguridad/Perfil.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useStyles from '../../theme/useStyles';
-import { Grid, Container, Typography } from '@material-ui/core';
+import { Grid, Container, Typography, Icon, Avatar, TextField, Divider, Button } from '@material-ui/core';
+import ImageUploader from 'react-images-upload';
 
 const Perfil = () => {
     const classes = useStyles();
@@ -11,6 +12,60 @@ const Perfil = () => {
                     <Typography variant='h5' className={classes.text_title}>
                         PERFIL DE USUARIO
                     </Typography>
+                    <form onSubmit={(e)=> e.preventDefault()}
+                    className={classes.form}
+                    >
+                     <ImageUploader 
+                     withIcon={false}
+                     buttonStyles={{borderRadius: '50%', padding:10, margin:0,position:'absolute',
+                    bottom:15, left:15
+                     }}
+                     className={classes.imageUploader}
+                     buttonText={<Icon>add_a_photo</Icon>}
+                     label={<Avatar alt='mi perfil' className={classes.avatarPerfil}/>}
+                     src='https://tottope.vteximg.com.br/arquivos/ids/167188-1000-1000/PILIGRAM-H-1810-V07_A.png?v=636723781789170000'
+                     imgExtension={['.jpg','.gif','.png','.jpeg']}
+                     maxFileSize={5242880}/>
+                     <TextField
+                     label='Nombre'
+                     variant='outlined'
+                     fullWidth
+                     className={classes.gridmb}
+                     value={'John'}
+                     />
+                    <TextField
+                    label='Apellidos'
+                    variant='outlined'
+                    fullWidth
+                    className={classes.gridmb}
+                    value={'Peralta'}
+                    />
+                    <TextField
+                    label='Correo Electronico'
+                    variant='outlined'
+                    fullWidth
+                    className={classes.gridmb}
+                    value={'john@gmail.com'}
+                    />
+                     <Divider className={classes.divider}/>
+                     <TextField
+                     label='Password'
+                     variant='outlined'
+                     fullWidth
+                     className={classes.gridmb}
+
+                     />
+                     <Divider className={classes.divider}/>
+                     <TextField
+                     label='Confirmar Password'
+                     variant='outlined'
+                     fullWidth
+                     className={classes.gridmb}
+
+                     />
+                     <Button variant='contained' color='primary'>ACTUALIZAR</Button>
+
+                   </form>
 
                 </Grid>
                 <Grid item md={9} xs={12}>
