15a836b35e8a860c978792d06946787eb0c03ec0
facundomarcos <facundomarcos@live.com.ar>
2024-05-31 11:57:05 -0300

    integracion con backend

--- src/componentes/pantallas/Productos.js
+++ src/componentes/pantallas/Productos.js
@@ -1,12 +1,29 @@
 import { Avatar, Button, Card, CardContent, CardMedia, Container, Grid, Typography } from '@material-ui/core';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import useStyles from '../../theme/useStyles';
 import { productoArray } from '../data/dataPrueba';
+import { getProductos } from '../../actions/ProductoAction';
 
 const Productos = (props) => {
 
-    console.log('REACT_APP_URL_BASE', process.env.REACT_APP_URL_BASE);
+    //console.log('REACT_APP_URL_BASE', process.env.REACT_APP_URL_BASE);
+
+    const [paginadorProductos, setPaginadorProductos] = useState({
+        count : 0,
+        pageIndex : 0,
+        pageSize : 0,
+        pageCount : 0,
+        data :[]
+    });
+
+    useEffect(()=>{
+        const getListaProductos = async () => {
+            const response = await getProductos();
+            setPaginadorProductos(response.data);
+        }
+
+        getListaProductos();
+    },[]);
 
     const miArray = productoArray; 
     const verProducto = (id) => {
@@ -14,12 +31,16 @@ const Productos = (props) => {
     }
     
     const classes = useStyles();
+
+    if(!paginadorProductos.data){
+        return null;
+    }
     return (
         
         <Container className={classes.containermt}>
             <Typography variant="h4" className={classes.text_title}>Productos</Typography>
             <Grid container spacing={4}>
-                { miArray.map(data => (
+                { paginadorProductos.data.map(data => (
                 <Grid item lg={3} md={4} sm={6} xs={12} key={data.key}>
                     <Card>
                         <CardMedia
@@ -33,13 +54,13 @@ const Productos = (props) => {
                         </CardMedia>
                         <CardContent>
                             <Typography  className={classes.text_card}>
-                                {data.titulo}
+                                {data.nombre}
                             </Typography>
                             <Button
                             variant="contained"
                             color="primary"
                             fullWidth
-                            onClick={() => verProducto(data.key)}
+                            onClick={() => verProducto(data.id)}
                             >
                                 MAS DETALLES
                             </Button>
